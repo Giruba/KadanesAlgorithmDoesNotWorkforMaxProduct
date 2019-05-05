61f23a9f4ef3b8197082ab0d8e93023e2d9917a0
Giruba Beulah SE <gibeulah@microsoft.com>
2019-05-05 23:21:10 +0530

    Add project files.

---
+++ MaximumProductSubarray/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MaximumProductSubarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Maximum Product Subarray!");
+            Console.WriteLine("-------------------------");
+
+            try
+            {
+                int numberElements = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the elements of the array separated by" +
+                    " space, comma or semi-colon");
+                int[] array = new int[numberElements];
+                String[] elements = Console.ReadLine().Split(' ', ',');
+                for (int index = 0; index < numberElements; index++) {
+                    array[index] = int.Parse(elements[index]);
+                }
+                PrintMaxProductSubArray(array);
+            }
+            catch (Exception exception) {
+                Console.WriteLine("Thrown exception is "+exception.Message);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void PrintMaxProductSubArray(int[] array) {
+            int maxProductEndingHere = array[0];
+            int maxProductSoFar = array[0];
+
+            for (int index = 1; index < array.Length; index++) {
+                maxProductEndingHere *= array[index];
+
+                if (maxProductEndingHere > maxProductSoFar) {
+                    maxProductSoFar = maxProductEndingHere;
+                }
+            }
+            Console.WriteLine("Maximum product in a subarray is "+maxProductSoFar);
+        }
+    }
+}
